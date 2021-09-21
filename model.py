c9806eadb4041446e095e57f2931431db13dcf97
kitae0522 <kitae0522@naver.com>
2021-09-21 20:34:37 +0900

    hi

--- model.py
+++ model.py
@@ -7,28 +7,29 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+import math
+import random
 import numpy as np
 from collections import deque
 
 class DQN:
     def __init__(self):
     	# Setting Hyper Parameters
-        self.epsilon_start = 0.9
+        self.epsilon_start = 0.99
         self.epsilon_end = 0.05
         self.epsilon_decay = 200
-        self.gamma = 0.95
+        self.gamma = 0.85
         self.lr = 1e-3
         self.batch_size = 64
 
-        ## Layer Model ##
-        self.linear_1 = nn.Linear(in_features=7, out_features=256)
-        self.linear_2 = nn.Linear(in_features=256, out_features=3)
-
         self.model = nn.Sequential(
-        	self.linear_1, nn.ReLU(), self.linear_2
+            nn.Linear(in_features=7, out_features=1024),
+            nn.ReLU(),
+            nn.Linear(in_features=1024, out_features=3)
     	)
 
         self.optimizer = optim.Adam(self.model.parameters(), self.lr)
+        self.criterion = nn.MSELoss()
         self.steps_done = 0
         self.epi_for_memory = deque(maxlen=10000)
     
@@ -39,24 +40,26 @@ class DQN:
                             torch.FloatTensor([reward]),
                             torch.FloatTensor([next_state])))
     
-    def select_acttion(self, state):
+    def select_action(self, state):
         # 지수함수를 이용하여 Epsilon의 값이 점점 Decay됨
-        epsilon_threshold = self.epsilon_end + (self.epsilon_start - self.epsilon_end) * np.exp(-1. * self.steps_done / self.epsilon_decay)
+        epsilon_threshold = self.epsilon_end + (self.epsilon_start - self.epsilon_end) * \
+                            np.exp(-1. * self.steps_done / self.epsilon_decay)
         self.steps_done += 1
 
         # (Decaying) Epsilon-Greedy Algorithm
-        if np.random.random() > epsilon_threshold:
-            return self.model(state).data.max(1)[1].view(1, 1)
+        if torch.rand(1)[0] > epsilon_threshold:
+            with torch.no_grad():
+                return self.model(state).data.max(1)[1].view(1, 1)
         else:
-            return torch.LongTensor([[np.random.randint(2, size=1)]])
+            return torch.LongTensor([[random.randrange(2)]])
 
-    def train(self):
+    def optimize_model(self):
         # 저장해 놓은 메모리 사이즈가 배치 사이즈보다 작으면 학습 안함
         if len(self.epi_for_memory) < self.batch_size:
             return
 
         # 저장해 놓은 메모리 사이즈를 배치 사이즈만큼 Sample로 뽑음
-        batch = np.random.choice(self.epi_for_memory, self.batch_size)
+        batch = random.sample(self.epi_for_memory, self.batch_size)
         states, actions, rewards, next_states = zip(*batch)
 
         states = torch.cat(states)
@@ -65,7 +68,6 @@ class DQN:
         next_states = torch.cat(next_states)
 
         current_q = self.model(states).gather(1, actions)
-        
         max_next_q = self.model(next_states).detach().max(1)[0]
 
         # Bellman equation
@@ -74,7 +76,8 @@ class DQN:
         
         # loss func = MSE (Mean Square Error)
         # [before update current Q-value - after update currnet Q-value]^2
-        loss = F.mse_loss(current_q.squeeze(), expected_q)
         self.optimizer.zero_grad()
+        loss = self.criterion(current_q.squeeze(), expected_q)
         loss.backward()
+
         self.optimizer.step()
--- snake.py
+++ snake.py
@@ -3,163 +3,165 @@ import random
 import numpy as np
 from pygame import draw
 from numpy import array as Vec
-
+from model import DQN
+import torch
 import copy
 
-WHITE=(255,255,255)
-BLACK=(0,0,0)
-RED=(255,0,0)
-GREEN=(0,255,0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
-BoardX=15
-BoardY=15
+BoardX = 15 #게임 사이즈
+BoardY = 15
 
-PixelPerBlock=50
+PixelPerBlock = 50
 
-screen=pygame.display.set_mode((BoardX*PixelPerBlock,BoardY*PixelPerBlock),pygame.DOUBLEBUF)
-clock=pygame.time.Clock()
+screen = pygame.display.set_mode(
+    (BoardX*PixelPerBlock, BoardY*PixelPerBlock), pygame.DOUBLEBUF)
+clock = pygame.time.Clock()
 
-framerate=10
-speed=10
+framerate = 10
+speed = 10
 
 
-delta=[
-    Vec([1,0]), #Right
-    Vec([0,1]), #Down
-    Vec([-1,0]),#Left
-    Vec([0,-1]) #Up
+delta = [ #방향당 위치 변화값
+    Vec([1, 0]),  # Right
+    Vec([0, 1]),  # Down
+    Vec([-1, 0]),  # Left
+    Vec([0, -1])  # Up
 ]
 
-KEY2DIR={
-    pygame.K_UP:3,
-    pygame.K_DOWN:1,
-    pygame.K_LEFT:2,
-    pygame.K_RIGHT:0
+KEY2DIR = { 
+    pygame.K_UP: 3,
+    pygame.K_DOWN: 1,
+    pygame.K_LEFT: 2,
+    pygame.K_RIGHT: 0
 }
 
-def Opposite(dir): 
-    return (dir+2)%4
 
-def getDir(dir):
-    foward=dir
-    right=(dir+1)%4
-    left=(dir-1)%4
-    return [left,foward,right]
+def Opposite(dir): #정면방향 기준 뒤쪽 방향 (절대방향) 리턴
+    return (dir+2) % 4
 
-def DrawBlock(position,color):
-    block=pygame.Rect(position[0]*PixelPerBlock,position[1]*PixelPerBlock,PixelPerBlock,PixelPerBlock)
-    pygame.draw.rect(screen,color,block)
+
+def getDir(dir): #정면방향 기준 [좌측, 정면, 우측] (절대방향) 리턴
+    foward = dir
+    right = (dir+1) % 4
+    left = (dir-1) % 4
+    return [left, foward, right]
+
+
+def DrawBlock(position, color):
+    block = pygame.Rect(position[0]*PixelPerBlock, position[1]
+                        * PixelPerBlock, PixelPerBlock, PixelPerBlock)
+    pygame.draw.rect(screen, color, block)
+
 
 class Snake:
-    def __init__(self):
-        self.body=[Vec([BoardX//2,BoardY//2]),Vec([BoardX//2,BoardY//2])]
-        self.dir=0
-        self.last_dir=0
+    def __init__(self): #게임 초기상태
+        self.body = [Vec([BoardX//2, BoardY//2]), Vec([BoardX//2, BoardY//2])]
+        self.dir = 0
+        self.last_dir = 0
         self.genApple()
-                
-    def Draw(self):
+
+    def Draw(self): #화면 출력
         for position in self.body:
-            DrawBlock(position,WHITE)
-        DrawBlock(self.apple,RED)
-        
-    def MoveSnake(self):
-        head=self.body[0]+delta[self.dir]
-        self.last_dir=self.dir
-        self.body.insert(0,head)
-        if np.array_equal(head,self.apple):
+            DrawBlock(position, WHITE)
+        DrawBlock(self.apple, RED)
+
+    def MoveSnake(self): #현재 dir로 이동
+        head = self.body[0]+delta[self.dir]
+        self.last_dir = self.dir
+        self.body.insert(0, head)
+        if np.array_equal(head, self.apple):
             self.genApple()
         else:
             self.body.pop()
-    
-    def isDead(self):
-        head=self.body[0]
-        if head[0]<0 or head[0]>=BoardX or head[1]<0 or head[1]>=BoardY:
+
+    def isDead(self): #죽었는지 확인
+        head = self.body[0]
+        if head[0] < 0 or head[0] >= BoardX or head[1] < 0 or head[1] >= BoardY:
             return True
-        for i in range(1,len(self.body)):
-            if np.array_equal(self.body[0],self.body[i]):
+        for i in range(1, len(self.body)):
+            if np.array_equal(self.body[0], self.body[i]):
                 return True
         return False
-    
-    def changeDir(self,dir_NEW):
+
+    def changeDir(self, dir_NEW): #절대방향으로 방향 변경
         if Opposite(self.last_dir) == dir_NEW or self.last_dir == dir_NEW:
             return False
-        self.dir=dir_NEW
+        self.dir = dir_NEW
         return True
-    
-    def genApple(self):
+
+    def genApple(self): #사과 생성
         while True:
-            temp=random.randrange(0,BoardX*BoardY)
-            apple=Vec([temp%BoardX,temp//BoardY])
-            coll=False
+            temp = random.randrange(0, BoardX*BoardY)
+            apple = Vec([temp % BoardX, temp//BoardY])
+            coll = False
             if self.isInBody(apple):
                 continue
-            self.apple=apple
+            self.apple = apple
             return
-    
-    def getState(self):
-        head=self.body[0]
-        dir=getDir(self.dir)+[Opposite(self.dir)] #left,foward,right,back
-        distance=[1,1,1]
-        done=[False,False,False]
-        pos=copy.deepcopy([head,head,head])
+
+    def getState(self): #현재 state 리턴  
+        head = self.body[0]
+        dir = getDir(self.dir)+[Opposite(self.dir)]  # left,foward,right,back
+        distance = [1, 1, 1]
+        done = [False, False, False]
+        pos = copy.deepcopy([head, head, head])
         for i in range(5):
             for j in range(3):
                 if done[j]:
                     continue
-                pos[j]+=delta[dir[j]]
+                pos[j] += delta[dir[j]]
                 if self.isInBody(pos[j]) or self.isOutOfBoard(pos[j]):
-                    done[j]=True
+                    done[j] = True
                 else:
-                    distance[j]-=0.2
-        appledir=[0,0,0,0]
-        toapple=self.apple-head
+                    distance[j] -= 0.2
+        appledir = [0, 0, 0, 0]
+        toapple = self.apple-head
         for i in range(4):
-            if np.inner(toapple,delta[dir[i]])>0:
-                appledir[i]=1
-        return distance+appledir
-        
-    def isOutOfBoard(self,position):
-        if position[0]<0 or position[0]>=BoardX or position[1]<0 or position[1]>=BoardY:
+            if np.inner(toapple, delta[dir[i]]) > 0:
+                appledir[i] = 1
+        return torch.FloatTensor(distance+appledir)
+
+    def isOutOfBoard(self, position): #해당 좌표가 보드 밖으로 나갔는지 확인
+        if position[0] < 0 or position[0] >= BoardX or position[1] < 0 or position[1] >= BoardY:
             return True
         return False
-        
-    def isInBody(self,position):
+
+    def isInBody(self, position): #해당 좌표가 뱀의 몸에 겹치는지 확인
         for elem in self.body:
-            if np.array_equal(elem,position):
+            if np.array_equal(elem, position):
                 return True
         return False
-        
-        
+
+def MainLoop(episode=50):
+    Game = Snake() 
+    agent = DQN()
     
-def MainLoop():
-    Game=Snake()
-    running=True
-    last_input=[]
+    running = True
     while running:
-        clock.tick(framerate)
-        screen.fill(BLACK)
-        for dir in last_input:
-            if Game.changeDir(dir):
-                break
-        last_input.clear()
-        gotInput=False
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                running=False
-            if event.type==pygame.KEYDOWN:
-                if event.key in KEY2DIR:
-                    if gotInput:
-                        last_input.append(KEY2DIR[event.key])
-                    else:
-                        temp=Game.changeDir(KEY2DIR[event.key])
-                        gotInput=True or temp is True
-        Game.MoveSnake()
-        if Game.isDead():
-            Game.__init__()
-        print(Game.getState())
-        Game.Draw()
-        pygame.display.flip()
-        
-        
+        for i in range(episode):
+            clock.tick(framerate)  #딜레이
+            screen.fill(BLACK)
+            for event in pygame.event.get(): #종료시 loop break
+                if event.type == pygame.QUIT:
+                    running = False
+            
+            dirs = getDir(Game.dir)
+            try: Game.changeDir(dirs[agent.select_action(Game.getState())])
+            except: Game.changeDir(dirs[1])
+            Game.MoveSnake() #현재 방향으로 이동
+
+            # if Game.
+
+            agent.memorize(Game.getState(), agent.select_action(Game.getState()), reward, next_state)
+
+            if Game.isDead(): #뒤지면 초기화
+                Game.__init__()
+            Game.Draw()
+            pygame.display.flip()
+
+
 MainLoop()
-        
